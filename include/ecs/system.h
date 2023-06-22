0e553aa69d2ecb11bbada7c797e1acee30a2f40a
Dmytro Zykov <dmytri.zykov@gmail.com>
2023-06-23 02:03:54 +0300

    Implement basic sprite render system

--- include/ecs/system.h
+++ include/ecs/system.h
@@ -1,12 +1,13 @@
 #pragma once
 
+#include <cassert>
 #include <entt/entt.hpp>
 
 namespace ecs {
 
 class System {
 public:
-    virtual void Update(entt::registry &registry) = 0;
+    virtual void Update(entt::registry &registry) noexcept = 0;
 };
 
 }  // namespace ecs
