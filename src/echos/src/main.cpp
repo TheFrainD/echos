16bddcf4315ea4ba7889fae6a95fb40804949caf
Dmytro Zykov <dmytri.zykov@gmail.com>
2023-07-31 15:31:19 +0300

    Change project structure

--- src/echos/src/main.cpp
+++ src/echos/src/main.cpp
@@ -2,12 +2,8 @@
 
 #include "core/game.h"
 
-using namespace entt::literals;
-
 int main(int argc, char **argv) {
     const auto game {std::make_unique<core::Game>(800, 600, "Echos")};
-
     game->Run();
-
     return 0;
 }
